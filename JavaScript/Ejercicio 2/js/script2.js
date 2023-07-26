a0e3e64729746c5fdaa9eb2eeb38395138448709
Andres <quevedo0504@gmail.com>
2023-07-25 21:40:58 -0500

    Ejercicios JS

---
+++ JavaScript/Ejercicio 2/js/script2.js
@@ -0,0 +1,17 @@
+/* EJERCICIO 2 - Dado el sueldo de un empleado, imprimir en una alerta el nuevo sueldo del empleado si tuvo un aumento del 10%
+
+let sueldo = prompt ("Ingrese el valor del sueldo");
+let psueldo = 10/100;
+let nsueldo = parseInt(sueldo)+parseInt(sueldo*psueldo);
+alert("El nuevo sueldo con el aumento es: " +nsueldo);*/
+
+/* EJERCICIO 3 - Un vendedor que tiene su sueldo base, recibe una comision del 30% del total de las ventas del mes, el quiere saber cuanto ganará en un mes que tuvo 3 ventas
+
+let sueldo = prompt("Ingrese el valor del sueldo base")
+let ventas = prompt("Ingrese valor de ventas")
+let pcomision = 30/100;
+let comision = parseInt(ventas*pcomision)
+let nsueldo = parseInt(comision)+parseInt(sueldo)
+alert("Las ganancias del mes es: " +nsueldo)*/
+
+/* EJERCICIO 4 - 
